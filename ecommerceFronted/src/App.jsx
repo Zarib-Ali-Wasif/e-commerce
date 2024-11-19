f8680498fbe4c3749f6c615c97e9a02d65fbab6e
Zarib Ali Wasif <zaribaliwasif140@gmail.com>
2024-11-19 22:54:03 +0500

    re-setup the project

--- ecommerceFronted/src/App.jsx
+++ ecommerceFronted/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import AppRoutes from "./routes/AppRoutes";
+// import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <div>
-      <AppRoutes />
+      <h1>App</h1>
+      {/* <AppRoutes /> */}
     </div>
   );
 }
