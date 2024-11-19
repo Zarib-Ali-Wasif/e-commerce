4d773b13dab4448c7167c4be8cfcb9d6fdae7220
Zarib Ali Wasif <zaribaliwasif140@gmail.com>
2024-11-19 22:04:26 +0500

    update code

--- ecommerceBackend/src/main.ts
+++ ecommerceBackend/src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for all origins and methods
+  app.enableCors({
+    origin: '*', // Allows requests from all origins
+    methods: '*', // Allows all HTTP methods
+  });
+
   await app.listen(3000);
-  console.log(`Application is running on: 3000`);
+  console.log(`Application is running on: http://localhost:3000`);
 }
 bootstrap();
