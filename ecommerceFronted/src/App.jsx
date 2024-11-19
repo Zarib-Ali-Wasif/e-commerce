ea4f2db56a6035348ad47211a7f6db06bb3368e9
Zarib Ali Wasif <zaribaliwasif140@gmail.com>
2024-11-19 22:55:19 +0500

    remove commented code

--- ecommerceFronted/src/App.jsx
+++ ecommerceFronted/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-// import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <div>
       <h1>App</h1>
-      {/* <AppRoutes /> */}
     </div>
   );
 }
