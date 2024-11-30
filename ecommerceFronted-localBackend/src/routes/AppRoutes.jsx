4000f2fb748b8c64501a6c6b5f3de706786b716e
Zarib Ali Wasif <zaribaliwasif140@gmail.com>
2024-12-01 04:17:50 +0500

    routes protection

--- ecommerceFronted-localBackend/src/routes/AppRoutes.jsx
+++ ecommerceFronted-localBackend/src/routes/AppRoutes.jsx
@@ -10,7 +10,7 @@ import Cart from "../components/Cart";
 import Checkout from "../components/Checkout";
 import OrderConfirmation from "../components/OrderConfirmation";
 import Login from "../pages/Login";
-import Signup from "../pages/Signup"; // Import Signup page
+import Signup from "../pages/Signup";
 import ForgetPassword from "../pages/ForgetPassword";
 import AddProduct from "../pages/AddProduct";
 import OtpVerification from "../pages/OtpVerification";
@@ -18,15 +18,20 @@ import UpdatePassword from "../pages/UpdatePassword";
 import ResetPassword from "../pages/ResetPassword";
 
 // ProtectedRoute Component
-// const ProtectedRoute = ({ user, children }) => {
-//   return user ? children : <Navigate to="/login" replace />;
-// };
+const ProtectedRoute = ({ user, children }) => {
+  return user ? children : <Navigate to="/login" replace />;
+};
 
 function AppRoutes() {
-  // const { user } = useContext(AuthContext); // Fetch authenticated user from context
+  const user = localStorage.getItem("user");
 
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <Layout>
         <Routes>
           {/* Public Routes */}
@@ -34,42 +39,34 @@ function AppRoutes() {
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:id" element={<Products showModal={true} />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forget-password" element={<ForgetPassword />} />
           <Route path="/add-product" element={<AddProduct />} />
-          <Route path="/verify-otp" element={<OtpVerification />} />
-          <Route path="/update-password" element={<UpdatePassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
           <Route path="/otp-verification" element={<OtpVerification />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
 
           {/* Protected Routes */}
           <Route
-            path="/cart"
+            path="/update-password"
             element={
-              // <ProtectedRoute user={user}>
-              <Cart />
-              // </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/checkout"
-            element={
-              // <ProtectedRoute user={user}>
-              <Checkout />
-              // </ProtectedRoute>
+              <ProtectedRoute user={user}>
+                <UpdatePassword />
+              </ProtectedRoute>
             }
           />
           <Route
             path="/order-confirmation"
             element={
-              // <ProtectedRoute user={user}>
-              <OrderConfirmation />
-              // </ProtectedRoute>
+              <ProtectedRoute user={user}>
+                <OrderConfirmation />
+              </ProtectedRoute>
             }
           />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
     </BrowserRouter>
