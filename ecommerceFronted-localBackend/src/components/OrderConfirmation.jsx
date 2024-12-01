121f46d269092d816089ac4a539301dcc18dec55
Zarib Ali Wasif <zaribaliwasif140@gmail.com>
2024-12-01 06:08:41 +0500

    update template

--- ecommerceFronted-localBackend/src/components/OrderConfirmation.jsx
+++ ecommerceFronted-localBackend/src/components/OrderConfirmation.jsx
@@ -7,10 +7,13 @@ import {
   Card,
   CardContent,
   Divider,
+  IconButton,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { useSelector } from "react-redux";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const OrderConfirmation = () => {
   const navigate = useNavigate();
@@ -18,7 +21,6 @@ const OrderConfirmation = () => {
   const { productsList } = useSelector((state) => state.products);
 
   useEffect(() => {
-    // Retrieve order details from localStorage
     const orderData = JSON.parse(localStorage.getItem("order"));
     setOrder(orderData);
   }, []);
@@ -34,18 +36,18 @@ const OrderConfirmation = () => {
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
-          textAlign: "center",
+          alignItems: "center",
           padding: "50px",
           minHeight: "70vh",
           mt: 15,
         }}
       >
         <Typography variant="h4" fontWeight={"bold"} color="textSecondary">
-          You have not placed any orders yet.
+          Loading...
         </Typography>
-        ;
       </Box>
     );
+
   const {
     orderNumber,
     orderDate,
@@ -56,104 +58,139 @@ const OrderConfirmation = () => {
     orderItems,
     summary,
   } = order;
-  console.log(productsList);
 
   return (
-    <Box sx={{ mt: 20, textAlign: "center", mx: "auto", maxWidth: 800 }}>
-      <Typography variant="h4" fontWeight="bold" color="#1C4771" gutterBottom>
+    <Box sx={{ mt: 18, textAlign: "center", mx: "auto", maxWidth: 900 }}>
+      <CheckCircleIcon sx={{ fontSize: 80, color: "#1C4771", mb: 2 }} />
+      <Typography variant="h4" fontWeight="bold" color="#1C4771">
         Thank You for Your Order!
       </Typography>
       <Typography
         variant="body1"
         color="textSecondary"
-        maxWidth={350}
-        margin={"auto"}
-        mb={4}
+        sx={{ maxWidth: 600, mx: "auto", my: 2 }}
       >
         Your order has been successfully placed. We will notify you with updates
         soon.
       </Typography>
 
-      <Card elevation={3}>
+      <Card sx={{ borderRadius: 3, boxShadow: 3, mt: 4 }}>
         <CardContent>
           <Typography variant="h5" fontWeight="bold">
             Order Details
           </Typography>
           <Divider sx={{ my: 2 }} />
-          <Typography variant="body2">
-            <strong>Order Number:</strong> {order.orderNumber}
-          </Typography>
-          <Typography variant="body2">
-            <strong>Order Date:</strong>{" "}
-            {new Date(order.orderDate).toLocaleString()}
-          </Typography>
-          <Typography variant="body2">
-            <strong>Status:</strong> {order.status || "Pending"}
-          </Typography>
-          <Typography variant="body2">
-            <strong>Payment Method:</strong> {order.paymentMethod}
-          </Typography>
-          <Typography variant="body2">
-            <strong>Email:</strong> {order.email}
-          </Typography>
-          <Typography variant="body2">
-            <strong>Shipping Address:</strong> {order.address}
-          </Typography>
-          <Divider sx={{ my: 3 }} />
-
-          <Typography variant="h6" gutterBottom>
-            Ordered Items
-          </Typography>
-          {order.orderItems.map((item, index) => (
-            <Box
-              key={index}
+          <Grid container spacing={2}>
+            <Grid
+              item
+              xs={12}
+              md={7}
               sx={{
-                mb: 2,
-                // display: "flex",
-                justifyContent: "space-evenly",
-                alignItems: "center",
-                textAlign: "justify",
+                display: "flex",
+                flexDirection: "column",
+                textAlign: "left",
+                gap: 0.5,
+                margin: "0 auto",
+              }}
+            >
+              <Typography variant="body2" sx={{ overflowWrap: "none" }}>
+                <strong>Order Number:</strong> {orderNumber}
+              </Typography>
+              <Typography variant="body2">
+                <strong>Order Date:</strong>{" "}
+                {new Date(orderDate).toLocaleString()}
+              </Typography>
+              <Typography variant="body2">
+                <strong>Status:</strong> {status || "Pending"}
+              </Typography>
+              <Typography variant="body2">
+                <strong>Payment Method:</strong> {paymentMethod}
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={5}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                textAlign: "left",
+                gap: 0.5,
               }}
             >
               <Typography variant="body2">
-                <strong>Product:</strong>{" "}
-                {productsList.find((product) => product._id === item.productId)
-                  ?.title || item.productId}
+                <strong>Email:</strong> {email}
               </Typography>
               <Typography variant="body2">
-                <strong>Quantity:</strong> {item.quantity}
+                <strong>Shipping Address:</strong> {address}
               </Typography>
-              {/* <Typography variant="body2">
-                <strong>Price:</strong> ${item.price}
-              </Typography> */}
-              <Divider sx={{ my: 1 }} />
-            </Box>
-          ))}
+            </Grid>
+          </Grid>
+          <Divider sx={{ my: 3 }} />
 
-          <Typography variant="h6" sx={{ mt: 3 }}>
+          <Typography variant="h6" fontWeight="bold">
+            Ordered Items
+          </Typography>
+          <Grid container mt={3}>
+            {orderItems.map((item, index) => (
+              <Grid item xs={12} key={index}>
+                <Box
+                  sx={{
+                    p: 2,
+                    border: "1px solid #e0e0e0",
+                    borderRadius: 2,
+                    mb: 1,
+                    backgroundColor: "#f9f9f9",
+                    textAlign: "left",
+                  }}
+                >
+                  <Typography variant="body2">
+                    <strong>Product:</strong>{" "}
+                    <span style={{ color: "#757575" }}>
+                      {productsList.find(
+                        (product) => product._id === item.productId
+                      )?.title || item.productId}
+                    </span>
+                  </Typography>
+                  <Typography variant="body2">
+                    <strong>Quantity:</strong> {item.quantity}
+                  </Typography>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+
+          <Typography
+            variant="h6"
+            color="textSecondary"
+            sx={{ mt: 3, fontWeight: "600" }}
+          >
             Order Summary
           </Typography>
           <Typography variant="body2">
-            <strong>Subtotal:</strong> ${order.summary.subtotal}
+            <strong>Subtotal:</strong> ${summary.subtotal}
           </Typography>
           <Typography variant="body2">
-            <strong>Discount:</strong> ${order.summary.discount}
+            <strong>Discount:</strong> ${summary.discount}
           </Typography>
           <Typography variant="body2">
-            <strong>GST:</strong> {order.summary.gst}%
+            <strong>GST:</strong> {summary.gst}%
           </Typography>
-          <Typography variant="h6" sx={{ mt: 2 }}>
-            <strong>Total:</strong> ${order.summary.total}
+          <Typography variant="h5" sx={{ mt: 2, fontWeight: "bold" }}>
+            <strong>Total:</strong> ${summary.total}
           </Typography>
         </CardContent>
       </Card>
 
       <Button
         variant="contained"
+        startIcon={<ShoppingCartIcon />}
         sx={{
-          backgroundColor: "#1C4771",
           mt: 4,
           mb: 8,
+          px: 5,
+          py: 1.5,
+          backgroundColor: "#1C4771",
           color: "white",
           "&:hover": {
             backgroundColor: "#163b56",
