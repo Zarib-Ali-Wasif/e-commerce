efdb9ceebd636559b8a082685fe894f8922dce23
Zarib Ali Wasif <zaribaliwasif140@gmail.com>
2024-12-12 15:52:45 +0500

    update files

--- ecommerceBackend/src/main.ts
+++ ecommerceBackend/src/main.ts
@@ -1,16 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Environment Variables and Port
   const PORT = process.env.PORT || 3000;
-  // Enable CORS for all origins and methods
+
+  // CORS Configuration
   app.enableCors({
-    origin: '*', // Allows requests from all origins
-    methods: '*', // Allows all HTTP methods
+    origin: process.env.CORS_ORIGIN || '*', // Allows specific origins if defined; defaults to all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Common HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
+    credentials: true, // Allow credentials if needed
   });
 
-  await app.listen(PORT);
-  console.log(`Application is running on: http://localhost:${PORT}`);
+  // Error Handling and Initialization
+  try {
+    await app.listen(PORT);
+    Logger.log(
+      `🚀 Application is running on: http://localhost:${PORT}`,
+      'Bootstrap',
+    );
+  } catch (error: any) {
+    Logger.error(
+      `❌ Failed to start the application: ${error.message}`,
+      '',
+      'Bootstrap',
+    );
+    process.exit(1); // Exit the process with a failure code
+  }
 }
+
 bootstrap();
